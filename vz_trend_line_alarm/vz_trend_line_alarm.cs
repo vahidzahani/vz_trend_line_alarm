bc4f62b49a96e4ee343107009581152eab28bcd3
vahid <vahid.zahani@gmail.com>
2022-01-27 20:20:35 +0330

    first

---
+++ vz_trend_line_alarm/vz_trend_line_alarm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using cAlgo.API;
+using cAlgo.API.Internals;
+using cAlgo.API.Indicators;
+//vahid.zahani@gmail.com
+namespace cAlgo.Indicators
+{
+    [Indicator(IsOverlay = true, AccessRights = AccessRights.None)]
+    public class vz_trend_line_alarm : Indicator
+    {
+        public override void Calculate(int index)
+        {
+            if (IsLastBar)
+                DisplaySpreadOnChart();
+        }
+
+        private void DisplaySpreadOnChart()
+        {
+
+
+            var xx = "";
+            foreach (var ii in Chart.FindAllObjects<ChartTrendLine>())
+            {
+                if (ii.Comment == "ALARM")
+                    xx = xx + Math.Round(ii.Y1, Symbol.Digits) + " - " + Math.Round(ii.Y2, Symbol.Digits) + " - " + ii.Time1.Ticks + " - " + ii.Time2.Ticks + "\t" + ii.Comment + "\n";
+            }
+            ChartObjects.DrawText("mylabel", xx, StaticPosition.TopLeft, Colors.Yellow);
+        }
+    }
+}
