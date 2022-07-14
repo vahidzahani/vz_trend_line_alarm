6b7bce0b3627f562011d008817cb9fdbe6a132b7
vahid <vahid.zahani@gmail.com>
2022-07-14 18:10:54 +0430

    ok for draw  lines color

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vz_trend_line_alarm
 this is a trend line alarm algo for Ctrader users
+copy folder to source in 
+
+Document -  cAlgo - Source - Indicators - FOLDERNMAE
--- vz_trend_line_alarm/vz_trend_line_alarm.cs
+++ vz_trend_line_alarm/vz_trend_line_alarm.cs
@@ -8,23 +8,83 @@ namespace cAlgo.Indicators
     [Indicator(IsOverlay = true, AccessRights = AccessRights.None)]
     public class vz_trend_line_alarm : Indicator
     {
+        protected override void Initialize()
+        {
+            var stackPanel = new StackPanel 
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                BackgroundColor = Color.Gold,
+                Opacity = 0.5
+            };
+
+
+            var button = new Button 
+            {
+                Text = "my button",
+                Margin = 10
+            };
+
+            stackPanel.AddChild(button);
+            Chart.AddControl(stackPanel);
+
+        }
+
         public override void Calculate(int index)
         {
             if (IsLastBar)
+            {
                 DisplaySpreadOnChart();
+                // Notifications.PlaySound("C:\\ALARM.WAV");
+            }
         }
+
 
         private void DisplaySpreadOnChart()
         {
 
-
-            var xx = "";
-            foreach (var ii in Chart.FindAllObjects<ChartTrendLine>())
+            var color = Colors.Black;
+            var txt = "";
+            foreach (var t_line in Chart.FindAllObjects<ChartTrendLine>())
             {
-                if (ii.Comment == "ALARM")
-                    xx = xx + Math.Round(ii.Y1, Symbol.Digits) + " - " + Math.Round(ii.Y2, Symbol.Digits) + " - " + ii.Time1.Ticks + " - " + ii.Time2.Ticks + "\t" + ii.Comment + "\n";
+                if (t_line.Comment == "ALARM")
+                {
+                    var pipvalue = 1 / Symbol.PipSize;
+                    double Y1 = Math.Round(t_line.Y1, Symbol.Digits);
+                    double Y2 = Math.Round(t_line.Y2, Symbol.Digits);
+
+                    var X1 = Bars.OpenTimes.GetIndexByTime(t_line.Time1);
+                    var X2 = Bars.OpenTimes.GetIndexByTime(t_line.Time2);
+                    var X_live = Bars.OpenTimes.GetIndexByTime(Time);
+                    //var X_live = t_line.Time1.GetDateTimeFormats();
+
+                    double xDiff = X2 - X1;
+                    var yDiff = Y2 - Y1;
+                    //var yDiff = Math.Round(Y2 - Y1, Symbol.Digits);
+                    var pip = Math.Round(yDiff * pipvalue, 1);
+
+                    var price = Symbol.Bid;
+
+                    var shib = (yDiff) / (xDiff);
+                    //Math.Atan2(yDiff, xDiff) * 180.0 / Math.PI;
+                    var a = X_live * shib;
+
+
+                    txt = txt + a + " YDIFF:" + yDiff + " *** XLIVE:[" + X_live + "] " + " PRICE:[" + price + "] " + "PIPVALUE:(" + pipvalue + ") Y1:(" + Y1 + ") Y2:(" + Y2 + ") PIP:(" + pip + ") X1:(" + X1 + ")  X2:(" + X2 + ") Bar:(" + xDiff + ") SHIB:(" + shib + ")\n";
+                    if (Y1 < Y2)
+                    {
+                        t_line.Color = Color.Red;
+                    }
+                    else
+                    {
+                        t_line.Color = Color.Green;
+                    }
+
+                }
             }
-            ChartObjects.DrawText("mylabel", xx, StaticPosition.TopLeft, Colors.Yellow);
+
+            ChartObjects.DrawText("mylabel", txt, StaticPosition.TopLeft, color);
+
         }
     }
 }
